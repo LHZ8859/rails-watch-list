7f7f8c4b8b50918c24f818ab13160f01f5d65470
Lorenz Houze <lorenz.houze@gmail.com>
2025-05-23 10:59:54 +0200

    Add dotenv-rails

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,23 @@
 class BookmarksController < ApplicationController
+  before_action :set_list
 
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
-
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    
 
   end
 
@@ -18,6 +27,16 @@ class BookmarksController < ApplicationController
 
   def index
     @bookmarks = Bookmark.all
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
 end
